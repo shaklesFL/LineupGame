0949feaa21eb1ffaf4f2dc212052692287d3008a
alexparomenski <hockey23.97@gmail.com>
2020-12-07 22:01:05 -0500

    Drawing done

--- Assets/Scripts/Drawing/Draw.cs
+++ Assets/Scripts/Drawing/Draw.cs
@@ -26,12 +26,41 @@ public class Draw : MonoBehaviour
 
     Vector2 lastPos;
 
+    public GameObject brush_indicator;
+    private float brush_width = 0.07f;
+
+    // brush colours
+    public Color32 black_ind = new Color32(0,0,0,255);
+    public Color32 green_ind = new Color32(0, 255, 0, 255);
+    public Color32 blue_ind = new Color32(0, 0, 255, 255);
+    public Color32 red_ind = new Color32(255, 0, 0, 255);
+    public Color32 cyan_ind = new Color32(0, 255, 255, 255);
+    public Color32 yellow_ind = new Color32(255, 255, 0, 255);
+    public Color32 pink_ind = new Color32(255, 0, 255, 255);
+    public Color32 grey_ind = new Color32(230, 230, 230, 255);
+
+    private void Start()
+    {
+        brush_indicator.transform.localScale = new Vector2(0.05f, 0.05f);
+    }
+
 
     private void Update() {
+
+        Vector3 mousePos = Input.mousePosition;
+        brush_indicator.transform.position = mousePos;
+
         Drawing();
     }
 
-    public void Drawing() 
+    public void brushUpdate(float value) {
+        brush_indicator.transform.localScale = new Vector2((0.05f + (value * 0.40f)), (0.05f + (value * 0.40f)));
+        brush_width = 0.07f + (value * 0.7f);
+        Debug.Log("value" + value);
+
+    }
+
+    public void Drawing()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0)) {
             CreateBrush();
@@ -40,18 +69,19 @@ public class Draw : MonoBehaviour
             //m_camera.gameObject.transform.position.z -= 1;
 
             m_camera.gameObject.transform.position = m_camera.gameObject.transform.position + new Vector3(0, 0, -0.5f);
-        
+
+
         }
         if (Input.GetKey(KeyCode.Mouse0))
         {
             Vector2 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
-            if(mousePos != lastPos)
+            if (mousePos != lastPos)
             {
                 AddAPoint(mousePos);
                 lastPos = mousePos;
             }
         }
-        else 
+        else
         {
             currentLineRenderer = null;
         }
@@ -59,10 +89,11 @@ public class Draw : MonoBehaviour
 
     public void CreateBrush() {
         GameObject brushInstance = Instantiate(brush);
-    brushInstance.transform.parent = GameObject.FindGameObjectWithTag("LineHolder").transform;
-    currentLineRenderer = brushInstance.GetComponent<LineRenderer>();
+        brushInstance.transform.parent = GameObject.FindGameObjectWithTag("LineHolder").transform;
+        currentLineRenderer = brushInstance.GetComponent<LineRenderer>();
 
         ChooseColour(selected_colour);
+        setBrushSize();
         //currentLineRenderer.sortingOrder++;
         sorting_order++;
         Debug.Log("The sorting order is " + currentLineRenderer.sortingOrder);
@@ -74,6 +105,10 @@ public class Draw : MonoBehaviour
         currentLineRenderer.SetPosition(1, mousePos);
     }
 
+    public void setBrushSize() {
+        currentLineRenderer.startWidth = brush_width;
+    }
+
     public void AddAPoint(Vector2 pointPos) {
         currentLineRenderer.positionCount++;
         int positionIndex = currentLineRenderer.positionCount - 1;
@@ -83,34 +118,42 @@ public class Draw : MonoBehaviour
     public void ChangeBlack()
     {
         selected_colour = 0;
+        brush_indicator.GetComponent<Image>().color = black_ind;
     }
     public void ChangeGreen()
     {
         selected_colour = 1;
+        brush_indicator.GetComponent<Image>().color = green_ind;
     }
     public void ChangeBlue()
     {
         selected_colour = 2;
+        brush_indicator.GetComponent<Image>().color = blue_ind;
     }
     public void ChangeRed()
     {
         selected_colour = 3;
+        brush_indicator.GetComponent<Image>().color = red_ind;
     }
     public void ChangeCyan()
     {
         selected_colour = 4;
+        brush_indicator.GetComponent<Image>().color = cyan_ind;
     }
     public void ChangeYellow()
     {
         selected_colour = 5;
+        brush_indicator.GetComponent<Image>().color = yellow_ind;
     }
     public void ChangePink()
     {
         selected_colour = 6;
+        brush_indicator.GetComponent<Image>().color = pink_ind;
     }
     public void ChangeWhite()
     {
         selected_colour = 7;
+        brush_indicator.GetComponent<Image>().color = grey_ind;
     }
 
     public void ChooseColour(int colour)
@@ -148,7 +191,6 @@ public class Draw : MonoBehaviour
             case 7:
                 currentLineRenderer.material = white;
                 currentLineRenderer.sortingOrder = sorting_order;
-                currentLineRenderer.startWidth = 1;
                 //LineRenderer.Reset()
 
                 break;
---
+++ Assets/ShowSketch.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowSketch : MonoBehaviour
+{
+    public GameObject image;
+    private Sprite screenshot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Engine.settings.playerType == 1)
+        {
+            image.SetActive(false);
+            gameObject.SetActive(false);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnEnter() {
+
+        image.GetComponent<Image>().color = new Color32(255, 255, 255, 220);
+        string folderPath = Directory.GetCurrentDirectory() + "/Screenshots/Screenshot.png";
+        screenshot = IMG2Sprite.LoadNewSprite(folderPath, 100.0f);
+        image.GetComponent<Image>().sprite = screenshot;
+        
+    }
+
+    public void OnExit()
+    {
+        image.GetComponent<Image>().color = new Color32(255, 255, 255, 0);
+    }
+}
